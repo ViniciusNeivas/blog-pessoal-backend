9233a8f7b45f4ffbac236199d1cf833df2ed7520
ViniciusNeivas <91344925+ViniciusNeivas@users.noreply.github.com>
2022-03-25 20:49:43 -0300

    CRUD - Parte 2

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -2,10 +2,18 @@ package com.generation.blogpessoal.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,17 +22,73 @@ import com.generation.blogpessoal.repository.PostagemRepository;
 
 @RestController
 @RequestMapping("/postagens")
-@CrossOrigin(origins = "*", allowedHeaders = "*") 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PostagemController {
 
 	@Autowired
 	private PostagemRepository postagemRepository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Postagem>> getAll(){
+	public ResponseEntity<List<Postagem>> getAll() {
+
 		return ResponseEntity.ok(postagemRepository.findAll());
-		
-		//SELECT * FROM tb_postagens;
-		
+
+		// SELECT * FROM tb_postagens;
+
 	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id) {
+
+		return postagemRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+		// SELECT * FROM tb_postagens;
+		// lambda
+
+	}
+
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
+
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+
+	}
+
+	@PostMapping
+	public ResponseEntity<Postagem> postPostagem(@Valid @RequestBody Postagem postagem) {
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+
+		// Insert
+	}
+	/* Primeira forma de fazer
+	 @PutMapping
+	public ResponseEntity<Postagem> putPostagem(@Valid @RequestBody Postagem postagem){
+ 	return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+	return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+	 Insert
+	}*/
+
+	@PutMapping
+	public ResponseEntity<Postagem> putPostagem(@Valid @RequestBody Postagem postagem) {
+		return postagemRepository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.ok().body(postagemRepository.save(postagem)))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	/*Primeira forma de fazer
+	 @DeleteMapping("/{id}")
+	public void deletePostagem(@PathVariable Long id) {
+		postagemRepository.deleteById(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(postagemRepository.saveAll(id));
+	}*/
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletePostagem(@PathVariable Long id) {
+		return postagemRepository.findById(id)
+			.map(resposta -> {postagemRepository.deleteById(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();})
+			.orElse(ResponseEntity.notFound().build());
+	}
+
 }
--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -31,4 +31,38 @@ import org.hibernate.annotations.UpdateTimestamp;
 		@UpdateTimestamp
 		private LocalDateTime data;
 
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getTitulo() {
+			return titulo;
+		}
+
+		public void setTitulo(String titulo) {
+			this.titulo = titulo;
+		}
+
+		public String getTexto() {
+			return texto;
+		}
+
+		public void setTexto(String texto) {
+			this.texto = texto;
+		}
+
+		public LocalDateTime getData() {
+			return data;
+		}
+
+		public void setData(LocalDateTime data) {
+			this.data = data;
+		}
+		
+		
+		
 }
